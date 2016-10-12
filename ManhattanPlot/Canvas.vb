373b6fdd66d6ffea7ec82257ff8d36d9cd758721
xieguigang <xie.guigang@gmail.com>
2016-10-13 00:12:27 +0800

    using new imaging API from VisualBasic

--- ManhattanPlot/Canvas.vb
+++ ManhattanPlot/Canvas.vb
@@ -1,10 +1,15 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.Drawing.Drawing2D
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
-Imports Microsoft.VisualBasic
 
+''' <summary>
+''' The ``Manhattan Plot`` canvas
+''' </summary>
 Public Module Canvas
 
     ''' <summary>
@@ -112,13 +117,8 @@ Public Module Canvas
                          Optional equidistant As Boolean = False,
                          Optional relative As Boolean = False,
                          Optional ylog As String = "ln",
-                         Optional colorPattern As String = "chr") As Bitmap
-
-        Dim bmp As New Bitmap(width, height)
-
-        If margin.IsEmpty Then
-            margin = New Size(100, 150)
-        End If
+                         Optional colorPattern As String = "chr",
+                         Optional bg$ = "white") As Bitmap
 
         Dim log As Func(Of Double, Double) = GetValueMethod(ylog)
         Dim colorList As New Dictionary(Of String, Color)
@@ -143,160 +143,166 @@ Public Module Canvas
             Next
         End If
 
-        Using g As Graphics = Graphics.FromImage(bmp)
-            Dim serials As IEnumerable(Of String) = data.First.pvalues.Keys   ' 先绘制出系列的名称
-            Dim h As Integer = margin.Height
-            Dim left As Integer = width - 300
-            Dim font As New Font(FontFace.MicrosoftYaHei, 12, FontStyle.Regular)
-            Dim fsz As SizeF = g.MeasureString("0", font)
-            Dim sampleBrush As New Dictionary(Of String, SolidBrush)
+        Return g.GraphicsPlots(New Size(width, height), margin, "width",
+            Sub(ByRef g, region)
 
-            colorPattern = colorPattern.ToLower
-            g.CompositingQuality = Drawing2D.CompositingQuality.HighQuality
-            g.FillRectangle(Brushes.White, New Rectangle(New Point, New Size(width, height)))
+                Dim serials As IEnumerable(Of String) = data.First.pvalues.Keys   ' 先绘制出系列的名称
+                Dim h As Integer = margin.Height
+                Dim left As Integer = width - 300
+                Dim font As New Font(FontFace.MicrosoftYaHei, 12, FontStyle.Regular)
+                Dim fsz As SizeF = g.MeasureString("0", font)
+                Dim sampleBrush As New Dictionary(Of String, SolidBrush)
 
-            If colorPattern <> "samplename" Then ' 不是按照样品标注颜色的，则不再绘制legend
+                colorPattern = colorPattern.ToLower
+                g.CompositingQuality = CompositingQuality.HighQuality
+                g.FillRectangle(Brushes.White, New Rectangle(New Point, New Size(width, height)))
 
-            Else
-                For Each name As String In serials  ' legend绘制
-                    Dim br As SolidBrush = New SolidBrush(colorList(name))
+                If colorPattern <> "samplename" Then ' 不是按照样品标注颜色的，则不再绘制legend
 
-                    Call g.FillRectangle(br, New Rectangle(left, h, 100, fsz.Height))
-                    Call g.DrawString(If(String.IsNullOrEmpty(name), "null", name), font, Brushes.Black, New Point(left + 110, h))
+                Else
+                    For Each name As String In serials  ' legend绘制
+                        Dim br As New SolidBrush(colorList(name))
 
-                    h += fsz.Height + 10
-                    sampleBrush(name) = br
-                Next
-            End If
+                        Call g.FillRectangle(br, New Rectangle(left, h, 100, fsz.Height))
+                        Call g.DrawString(
+                            If(String.IsNullOrEmpty(name), "null", name),
+                            font,
+                            Brushes.Black,
+                            New Point(left + 110, h))
 
-            Dim gData = From x As SNP
-                        In data
-                        Select x
-                        Group x By x.Chr Into Group
-                        Order By Chr Ascending
+                        h += fsz.Height + 10
+                        sampleBrush(name) = br
+                    Next
+                End If
 
-            Dim total As Long = 0
-            Dim chrData As New List(Of NamedValue(Of SNP()))
+                Dim gData = From x As SNP
+                            In data
+                            Select x
+                            Group x By x.Chr Into Group
+                            Order By Chr Ascending
 
-            For Each chromsome In gData
-                Dim id As String = chromsome.Chr.ToUpper
-                Dim relLen As Integer() = chromsome.Group.ToArray(Function(x) x.Position)
+                Dim total As Long = 0
+                Dim chrData As New List(Of NamedValue(Of SNP()))
 
-                total += If(relative, relLen.Max - relLen.Min, Canvas.Chromosomes(id))
-                chrData += New NamedValue(Of SNP()) With {
-                    .Name = id,
-                    .x = LinqAPI.Exec(Of SNP) <= From x As SNP
-                                                 In chromsome.Group
-                                                 Select x
-                                                 Order By x.Position Ascending
+                For Each chromsome In gData
+                    Dim id As String = chromsome.Chr.ToUpper
+                    Dim relLen As Integer() = chromsome _
+                        .Group _
+                        .ToArray(Function(x) x.Position)
+
+                    total += If(relative, relLen.Max - relLen.Min, Canvas.Chromosomes(id))
+                    chrData += New NamedValue(Of SNP()) With {
+                        .Name = id,
+                        .x = LinqAPI.Exec(Of SNP) <= From x As SNP
+                                                     In chromsome.Group
+                                                     Select x
+                                                     Order By x.Position Ascending
                     }
-            Next
-
-            Dim xLeft As Integer = margin.Width
-            Dim maxY As Double = chrData _
-                .Select(Function(x) x.x _
-                .Select(Function(o) o.pvalues.Values) _
-                .MatrixAsIterator) _
-                .MatrixAsIterator _
-                .Where(Function(n) Not Double.IsNaN(n)).Min  ' pvalue越小则-log越大
-            maxY = log(maxY) + 1
-            chrData = New List(Of NamedValue(Of SNP()))(chrData.OrderBy(Function(x) Val(x.Name)))
-            font = New Font(FontFace.MicrosoftYaHei, 24, FontStyle.Regular)
-            ylog = ylog.ToLower
-
-            ' 绘制X轴
-            Call g.DrawLine(Pens.Black, New Point(margin.Width, height - margin.Height), New Point(width - margin.Width, height - margin.Height))
-            ' 绘制y轴
-            Call g.DrawLine(Pens.Black, New Point(margin.Width, height - margin.Height), New Point(margin.Width, margin.Height))
-
-            If ylog <> "ln" AndAlso ylog <> "log" Then
-                For iy As Double = 0 To maxY Step 0.25
-                    Dim y As Integer = height - (height - 2 * margin.Height) * (iy / maxY) - margin.Height
-
-                    fsz = g.MeasureString(iy, font)
-
-                    Call g.DrawLine(Pens.Black, New Point(margin.Width, y), New Point(margin.Width - 4, y))
-                    Call g.DrawString(iy, font, Brushes.Black, New Point(15, y - fsz.Height / 2))
                 Next
-            Else
-                For iy As Double = 1 To maxY
-                    Dim y As Integer = height - (height - 2 * margin.Height) * (iy / maxY) - margin.Height
 
-                    fsz = g.MeasureString(iy, font)
+                Dim xLeft As Integer = margin.Width
+                Dim maxY As Double = chrData _
+                    .Select(Function(x) x.x _
+                    .Select(Function(o) o.pvalues.Values) _
+                    .MatrixAsIterator) _
+                    .MatrixAsIterator _
+                    .Where(Function(n) Not Double.IsNaN(n)).Min  ' pvalue越小则-log越大
+                maxY = log(maxY) + 1
+                chrData = New List(Of NamedValue(Of SNP()))(chrData.OrderBy(Function(x) Val(x.Name)))
+                font = New Font(FontFace.MicrosoftYaHei, 24, FontStyle.Regular)
+                ylog = ylog.ToLower
 
-                    Call g.DrawLine(Pens.Black, New Point(margin.Width, y), New Point(margin.Width - 4, y))
-                    Call g.DrawString(iy, font, Brushes.Black, New Point(25, y - fsz.Height / 2))
-                Next
-            End If
+                ' 绘制X轴
+                Call g.DrawLine(Pens.Black, New Point(margin.Width, height - margin.Height), New Point(width - margin.Width, height - margin.Height))
+                ' 绘制y轴
+                Call g.DrawLine(Pens.Black, New Point(margin.Width, height - margin.Height), New Point(margin.Width, margin.Height))
 
-            Dim labelFont As New Font(FontFace.BookmanOldStyle, 16, FontStyle.Bold)
-            Dim ed As Integer = (width - 2 * margin.Width) / chrData.Count
-            Dim r As Double = ptSize / 2
-            Dim odds As Boolean = True
+                If ylog <> "ln" AndAlso ylog <> "log" Then
+                    For iy As Double = 0 To maxY Step 0.25
+                        Dim y As Integer = height - (height - 2 * margin.Height) * (iy / maxY) - margin.Height
 
-            For Each chromsome In chrData
-                Dim chrName As String = chromsome.Name
-                Dim relLen As Integer() = chromsome.x.ToArray(Function(x) x.Position)
-                Dim l As Integer = If(relative, relLen.Max - relLen.Min, Chromosomes(chrName))
-                Dim max As Integer = If(equidistant, ed, (width - 2 * margin.Width) * (l / total))  '  最大的长度
+                        fsz = g.MeasureString(iy, font)
 
-                If l = 0 AndAlso relLen.Length = 1 Then
-                    l = relLen(Scan0)
-                End If
-                If chrName = "23" Then
-                    chrName = "X"
-                ElseIf chrName = "24" Then
-                    chrName = "Y"
+                        Call g.DrawLine(Pens.Black, New Point(margin.Width, y), New Point(margin.Width - 4, y))
+                        Call g.DrawString(iy, font, Brushes.Black, New Point(15, y - fsz.Height / 2))
+                    Next
+                Else
+                    For iy As Double = 1 To maxY
+                        Dim y As Integer = height - (height - 2 * margin.Height) * (iy / maxY) - margin.Height
+
+                        fsz = g.MeasureString(iy, font)
+
+                        Call g.DrawLine(Pens.Black, New Point(margin.Width, y), New Point(margin.Width - 4, y))
+                        Call g.DrawString(iy, font, Brushes.Black, New Point(25, y - fsz.Height / 2))
+                    Next
                 End If
 
-                g.DrawLine(Pens.Black, New Point(xLeft + max, height - margin.Height), New Point(xLeft + max, height - margin.Height - 5))
-                fsz = g.MeasureString(chrName, font)
-                g.DrawString(chrName, font, Brushes.Black, New Point(xLeft + (max - fsz.Width) / 2, height - margin.Height + 15))
+                Dim labelFont As New Font(FontFace.BookmanOldStyle, 16, FontStyle.Bold)
+                Dim ed As Integer = (width - 2 * margin.Width) / chrData.Count
+                Dim r As Double = ptSize / 2
+                Dim odds As Boolean = True
 
-                For Each snp As SNP In chromsome.x
-                    Dim x As Integer = max * (If(relative, snp.Position - relLen.Min, snp.Position) / l) + xLeft
+                For Each chromsome In chrData
+                    Dim chrName As String = chromsome.Name
+                    Dim relLen As Integer() = chromsome.x.ToArray(Function(x) x.Position)
+                    Dim l As Integer = If(relative, relLen.Max - relLen.Min, Chromosomes(chrName))
+                    Dim max As Integer = If(equidistant, ed, (width - 2 * margin.Width) * (l / total))  '  最大的长度
 
-                    If colorPattern <> "chr" Then
-
-                        If snp.Position > l Then
-                            Continue For
-                        End If
+                    If l = 0 AndAlso relLen.Length = 1 Then
+                        l = relLen(Scan0)
+                    End If
+                    If chrName = "23" Then
+                        chrName = "X"
+                    ElseIf chrName = "24" Then
+                        chrName = "Y"
                     End If
 
-                    For Each sample In snp.pvalues.Where(Function(n) Not Double.IsNaN(n.Value))
-                        Dim y As Integer = height - (height - 2 * margin.Height) * ((log(sample.Value)) / maxY) - margin.Height
-                        Dim label As String = $"{snp.Gene} ({sample.Key})"
-                        Dim color As SolidBrush
+                    g.DrawLine(Pens.Black, New Point(xLeft + max, height - margin.Height), New Point(xLeft + max, height - margin.Height - 5))
+                    fsz = g.MeasureString(chrName, font)
+                    g.DrawString(chrName, font, Brushes.Black, New Point(xLeft + (max - fsz.Width) / 2, height - margin.Height + 15))
 
-                        Select Case colorPattern
-                            Case "chr"
-                                color = ChromosomeColors(snp.Chr.ToUpper)
-                            Case "samplename"
-                                color = sampleBrush(sample.Key)
-                            Case Else
-                                If odds Then
-                                    color = __odds
-                                Else
-                                    color = __evens
-                                End If
-                        End Select
+                    For Each snp As SNP In chromsome.x
+                        Dim x As Integer = max * (If(relative, snp.Position - relLen.Min, snp.Position) / l) + xLeft
 
-                        Call g.FillPie(color, New Rectangle(x - r, y - r, ptSize, ptSize), 0, 360)
+                        If colorPattern <> "chr" Then
 
-                        If showDebugLabel Then
-                            Call g.DrawString(label,
-                                              labelFont,
-                                              Brushes.Black,
-                                              New Point(x + ptSize + 3, y + 10))
+                            If snp.Position > l Then
+                                Continue For
+                            End If
                         End If
+
+                        For Each sample In snp.pvalues.Where(Function(n) Not Double.IsNaN(n.Value))
+                            Dim y As Integer = height - (height - 2 * margin.Height) * ((log(sample.Value)) / maxY) - margin.Height
+                            Dim label As String = $"{snp.Gene} ({sample.Key})"
+                            Dim color As SolidBrush
+
+                            Select Case colorPattern
+                                Case "chr"
+                                    color = ChromosomeColors(snp.Chr.ToUpper)
+                                Case "samplename"
+                                    color = sampleBrush(sample.Key)
+                                Case Else
+                                    If odds Then
+                                        color = __odds
+                                    Else
+                                        color = __evens
+                                    End If
+                            End Select
+
+                            Call g.FillPie(color, New Rectangle(x - r, y - r, ptSize, ptSize), 0, 360)
+
+                            If showDebugLabel Then
+                                Call g.DrawString(label,
+                                                  labelFont,
+                                                  Brushes.Black,
+                                                  New Point(x + ptSize + 3, y + 10))
+                            End If
+                        Next
                     Next
+
+                    xLeft += max
+                    odds = Not odds
                 Next
-
-                xLeft += max
-                odds = Not odds
-            Next
-        End Using
-
-        Return bmp
+            End Sub)
     End Function
 End Module
